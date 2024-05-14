f22510b9152b8b8b86052820d4d15f9c188923d2
nadiraaraa <nadiraaraa@gmail.com>
2024-05-14 20:38:38 +0700

    plain frontendddd

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Navbar currPage="Home"/>
-      <div className="z-10 w-full max-w-5xl items-center justify-between font- text-sm lg:flex p-24">
+      <div className="w-full items-center justify-between font- text-sm lg:flex p-24">
         
       </div>
     </main>
