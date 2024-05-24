f71abb77d88e1916c3a297b39688a1633fe16467
TimothySubekti0322 <18221063@std.stei.itb.ac.id>
2024-05-24 23:18:42 +0700

    refactor generate key type

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,11 +1,10 @@
-'use client';
+"use client";
 
 import { ChangeEvent, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import axios from 'axios';
+import axios from "axios";
 import { generateKey } from "../utils/generateRSA";
 import { checkNumberPositivePrime } from "../utils/function";
-
 
 interface InputType {
   name: string;
@@ -18,35 +17,42 @@ interface KeyType {
   modulus: String;
 }
 
+interface Key {
+  id: number;
+  publicKeyE: string;
+  publicKeyN: string;
+  owner: string;
+}
+
 export default function Generate() {
   const [loading, setLoading] = useState<boolean>(true);
   const [errorNum, setErrorNum] = useState("");
-  const [usedKeys, setUsedKeys] = useState([]);
+  const [usedKeys, setUsedKeys] = useState<Key[]>([]);
   const [errorName, setErrorName] = useState("");
   const [generated, setGenerated] = useState<boolean>(false);
   const [input, setInput] = useState<InputType>({
     name: "",
     num1: "",
-    num2: ""
+    num2: "",
   });
   const [key, setKey] = useState<KeyType>({
     publicKey: "",
     privateKey: "",
-    modulus: ""
+    modulus: "",
   });
 
   const validateSubmit = () => {
-    const isNameTaken = usedKeys.some(item => item.owner === input.name);
+    const isNameTaken = usedKeys.some((item) => item.owner === input.name);
 
-    if (input.name === '') {
-      setErrorName("Name can't be empty.")
+    if (input.name === "") {
+      setErrorName("Name can't be empty.");
     } else if (isNameTaken) {
-      setErrorName("Name is taken. Choose another name!")
+      setErrorName("Name is taken. Choose another name!");
     } else {
-      setErrorName('');
+      setErrorName("");
     }
 
-    if (input.num1 == '' || input.num2 == '') {
+    if (input.num1 == "" || input.num2 == "") {
       setErrorNum("Numbers can't be empty.");
       console.log("1");
     } else if (input.num1 === input.num2) {
@@ -57,76 +63,74 @@ export default function Generate() {
       const checkPrime1 = checkNumberPositivePrime(prime1);
       console.log("3");
       if (checkPrime1 !== "Number is prime") {
-        setErrorNum('Number 1 must be prime!');
+        setErrorNum("Number 1 must be prime!");
       } else {
         const prime2 = parseInt(input.num2);
         const checkPrime2 = checkNumberPositivePrime(prime2);
         if (checkPrime2 !== "Number is prime") {
-          setErrorNum('Number 2 must be prime!');
+          setErrorNum("Number 2 must be prime!");
         } else {
-          setErrorNum('');
+          setErrorNum("");
         }
       }
     }
     console.log(errorName, errorNum);
-
-  }
+  };
 
   const handleSubmit = async () => {
     validateSubmit();
     console.log(errorName, errorNum);
-    if (errorName==='' && errorNum===''){
+    if (errorName === "" && errorNum === "") {
       console.log(errorName, errorNum);
       const newKey = generateKey(input.num1, input.num2);
       console.log("newKey", newKey);
-        const form = ({
-          publicKeyE: newKey.publicKey.toString(),
-          publicKeyN: newKey.modulus.toString(),
-          owner: input.name
-        })
+      const form = {
+        publicKeyE: newKey.publicKey.toString(),
+        publicKeyN: newKey.modulus.toString(),
+        owner: input.name,
+      };
 
-        setKey({
-          publicKey: newKey.publicKey.toString(),
-          privateKey: newKey.privateKey.toString(),
-          modulus: newKey.modulus.toString()
-        })
+      setKey({
+        publicKey: newKey.publicKey.toString(),
+        privateKey: newKey.privateKey.toString(),
+        modulus: newKey.modulus.toString(),
+      });
 
-        console.log(form);
+      console.log(form);
 
-        try {
-          const response = await axios.post('/api/key', form);
-          const data = response.data;
-          if (data.status === 200) {
-              console.log('Submitted Data:', form);
-              setGenerated(true);
-          } else {
-              console.error('Failed to submit key:', data.message);
-              alert('Failed to submit key');
-          }
+      try {
+        const response = await axios.post("/api/key", form);
+        const data = response.data;
+        if (data.status === 200) {
+          console.log("Submitted Data:", form);
+          setGenerated(true);
+        } else {
+          console.error("Failed to submit key:", data.message);
+          alert("Failed to submit key");
+        }
       } catch (error) {
-          console.error('Error submitting key:', error);
-          alert('Failed to submit key');
-          setGenerated(false);
+        console.error("Error submitting key:", error);
+        alert("Failed to submit key");
+        setGenerated(false);
       }
-
     } else {
       setGenerated(false);
     }
-  }
+  };
 
   useEffect(() => {
     const fetchKey = async () => {
       try {
-        const response = await axios.get('/api/key');
+        const response = await axios.get("/api/key");
         const data = response.data;
         if (data.status === 200) {
           setUsedKeys(data.data);
-          console.log(data)
+          console.log(data);
         } else {
-          console.error('Failed to fetch keys:', data.message);
+          console.error("Failed to fetch keys:", data.message);
         }
       } catch (error) {
-        console.error('Error fetching keys:', error);
+        console.error("Error fetching keys:", error);
       }
     };
 
@@ -146,7 +150,9 @@ export default function Generate() {
       <Navbar currPage="Generate" />
       <div className="z-10 px-44 items-center font- text-sm py-24 px-16">
         <div className="text-center py-4">
-          <h1 className="text-purple1 text-lg font-bold">Input 2 random numbers</h1>
+          <h1 className="text-purple1 text-lg font-bold">
+            Input 2 random numbers
+          </h1>
         </div>
         <div className="items-center w-full">
           <label
@@ -201,14 +207,20 @@ export default function Generate() {
           </div>
         </div>
         <div className="py-8 flex justify-center">
-          <button className="bg-pink1 border-2 border- text-white1 text-md rounded-lg block w-60 p-2.5  hover:text-white drop-shadow-lg hover:drop-shadow-md hover:bg-pink2"
+          <button
+            className="bg-pink1 border-2 border- text-white1 text-md rounded-lg block w-60 p-2.5  hover:text-white drop-shadow-lg hover:drop-shadow-md hover:bg-pink2"
             onClick={handleSubmit}
           >
             Generate Key
           </button>
         </div>
 
-        <div className={(generated === true ? " " : "invisible ") + "flex justify-center gap-x-8 mt-2"}>
+        <div
+          className={
+            (generated === true ? " " : "invisible ") +
+            "flex justify-center gap-x-8 mt-2"
+          }
+        >
           <div className="items-center w-full">
             <p className="block  text-sm font-medium whitesspace-nowrap">
               Public Key
@@ -226,7 +238,6 @@ export default function Generate() {
             </div>
           </div>
         </div>
-
       </div>
     </main>
   );
