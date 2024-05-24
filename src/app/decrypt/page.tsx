821f16f4726cbd0f4de7062f4ecb185b2f65b62d
nadiraaraa <nadiraaraa@gmail.com>
2024-05-24 19:03:14 +0700

    decrypt in progress

--- src/app/decrypt/page.tsx
+++ src/app/decrypt/page.tsx
@@ -1,31 +1,39 @@
 'use client';
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import Navbar from "../components/Navbar";
-import { saveAs } from "file-saver";
+import GeneratePdfForm from "../puppeteer/GeneratePdfForm";
+import { handleAnyFileChange } from "../utils/decryptFile";
 
 export default function Decrypt() {
+  const [key, setKey] = useState("");
   const [fileName, setFileName] = useState("");
   const [fileBaseString, setFileBaseString] = useState<ArrayBuffer>();
   const [fileData, setFileData] = useState("");
 
-  const handleFileRead = async (e: ProgressEvent<FileReader>) => {
-    const content = e.target?.result;
-    const text = (content as string)
-      .split(/["\n"" "]/)
-      .join("")
-      .split(" ")
-      .join("");
-    setFileData(text);
-    console.log(text);
+  const handleInputChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+    setKey(value);
   };
+  // const handleFileRead = async (e: ProgressEvent<FileReader>) => {
+  //   const content = e.target?.result;
+  //   const text = (content as string)
+  //     .split(/["\n"" "]/)
+  //     .join("")
+  //     .split(" ")
+  //     .join("");
+  //   setFileData(text);
+  //   console.log(text);
+  // };
 
-  const downloadAnyFile = () => {
-    console.log(fileBaseString);
-    const file = new Blob([fileBaseString as ArrayBuffer], {
-      type: "pdf",
-    });
-    saveAs(file, fileName);
-  };
+  // const downloadAnyFile = () => {
+  //   console.log(fileBaseString);
+  //   const file = new Blob([fileBaseString as ArrayBuffer], {
+  //     type: "pdf",
+  //   });
+  //   saveAs(file, fileName);
+  // };
 
   return (
     <main className="min-h-screen items-center bg-gradient-to-b from-[#CAD8F0] to-blue2">
@@ -39,7 +47,16 @@ export default function Decrypt() {
                     <button className="bg-pink1 border-2 border- text-white1 text-md rounded-lg block w-60 p-2.5  hover:text-white drop-shadow-lg hover:drop-shadow-md hover:bg-pink2"
                     // onClick={() => } 
                     >
-                        Input Encrypted File
+                        <GeneratePdfForm />
+      <div className="mt-4">
+        <input
+          type="file"
+          className={`bg-[#fcf6e0] border-2 "border-[#cabc7d]  text-[#393432] text-sm rounded-lg focus:ring-[#E18679] focus:border-[#E18679] block w-full p-2.5`}
+          onChange={(event) =>
+            handleAnyFileChange(event, key)
+          }
+        />
+      </div>
                     </button>
                     <button className="bg-pink1 border-2 border- text-white1 text-md rounded-lg block w-60 p-2.5  hover:text-white drop-shadow-lg hover:drop-shadow-md hover:bg-pink2"
                     // onClick={() => } 
